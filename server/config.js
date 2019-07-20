d7bb13e9e73ab7c694b53e8f7c981ab5cd829012
Volodza <looka66@mail.ru>
2019-07-20 23:37:54 +0300

    config fix

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ config.port = process.env.WEB_PORT || 3000;
 
 config.vk.client_id = '7001016';
 config.vk.client_secret = 'hQD28VvPlYLgPC4tUbjl';
-config.vk.redirect_uri = `https://${config.hostname}/auth/vkontakte/callback`;
+config.vk.redirect_uri = `http://${config.hostname}/auth/vkontakte/callback`;
 
 config.mongo.url = "mongodb+srv://abc123:Vkfltytwjou9df9@vueexpress-nq8xi.mongodb.net/test?retryWrites=true&w=majority";
 config.mongo.name = 'vueExpress';
