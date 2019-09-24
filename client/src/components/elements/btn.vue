dd3092b46ca89a56f29bd09307b64ea972e2893f
VTshk <46195996+VTshk@users.noreply.github.com>
2019-09-24 16:39:07 +0300

    btn

---
+++ client/src/components/elements/btn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="btn">
+    <slot></slot>
+  </div>
+
+</template>
+
+<style lang="sass" scoped>
+  .btn
+    height: 20px
+    width: 50px
+    border: 1px solid #EDEEF0
+    border-radius: 2px
+    display: flex
+    justify-content: center
+    align-items: center
+    color: black
+    &:hover
+      border: 1px solid #CCCCCC
+    -moz-user-select: none
+    -khtml-user-select: none
+    -webkit-user-select: none
+    user-select: none
+
+
+</style>
+
