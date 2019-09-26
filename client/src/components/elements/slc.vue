031956439f11dfe3c52d748eb68c651515e14ef4
Volodza <looka66@mail.ru>
2019-09-26 12:44:04 +0300

    select

---
+++ client/src/components/elements/slc.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="select">
+    <div @click="isActive == !isActive" class="container">
+      <span v-if="selected">{{selected}}</span>
+      <span v-else>{{defaultText}}</span>
+    </div>
+
+    <div v-if="isActive" class="dropdown">
+      <ul>
+        <li 
+          v-for="(item,i) in items" 
+          :key="i"
+          @click="selected = item"
+        >
+          {{item}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    items:{
+      type:Array
+    }
+  },
+  data () {
+    return {
+      selected:null,
+      defaultText:'Выбрать...',
+      isActive:false
+    }
+  }
+
+}
+</script>
