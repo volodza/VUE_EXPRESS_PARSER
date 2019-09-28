770a6ce8f7b990d0478ffe3b7275d4cd9e403e25
Volodza <looka66@mail.ru>
2019-09-28 19:36:36 +0300

    checkbox added

---
+++ client/src/components/elements/PCheckbox.vue
@@ -0,0 +1,58 @@
+<template>
+  <div @click="check" class="checkbox">
+    <div class="square">
+      <div v-if="value == true" class="checkmark"></div>
+    </div>
+    <span>{{label}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    value:{
+      type:Boolean,
+      default:false
+    },
+    label:{
+      type:String,
+      default:'label'
+    }
+  },
+  data () {
+    return {}
+  },
+  methods:{
+    check(){
+      this.value = !this.value;
+      this.$emit('input',this.value)
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+  .checkbox
+    display: flex
+    cursor: pointer
+    margin: 2px
+    user-select: none
+
+  .square
+    height: 20px
+    width: 20px
+    border: 1px solid #d7d7d7
+    border-radius: 2px
+    margin-right: 10px
+
+  .checkmark
+    left: 5px
+    bottom: 1px
+    position: relative
+    width: 8px
+    height: 17px
+    border: solid grey
+    border-width: 0 2px 2px 0
+    -webkit-transform: rotate(45deg)
+    transform: rotate(45deg)
+</style>
